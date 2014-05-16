ebda07662e6f37f9923097471a81cb62237bf3a7
李俊波 <252500187@qq.com>
2014-05-16 14:23:56 +0800

    2014-05-16 controller

--- sls-parent/sls-webapp/src/main/java/com/sls/user/DealScormController.java
+++ sls-parent/sls-webapp/src/main/java/com/sls/user/DealScormController.java
@@ -5,6 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @Transactional
@@ -14,4 +19,10 @@ public class DealScormController {
     @Autowired
     private ScormService scormService;
 
+    @RequestMapping(value = "registerScorm", method = {RequestMethod.GET})
+    @ResponseBody
+    public String[] registerScorm(HttpServletRequest request, @RequestParam("scormId") String scormId) {
+        String str[] = {scormService.registerScorm(Integer.parseInt(scormId), request)};
+        return str;
+    }
 }
